0927e097b5629d63879f8f3028962c61e0836b60
Kateryna-Kupriianova <kupriyanov.forever@gmail.com>
2024-06-15 23:40:55 +0300

    create form

---
+++ src/main.js
@@ -0,0 +1,33 @@
+const searchForm = document.querySelector('form');
+console.log(searchForm);
+searchForm.style.display = 'flex';
+searchForm.style.justifyContent = 'center';
+searchForm.style.gap = '8px';
+searchForm.style.height = '40px';
+searchForm.style.width = '371px';
+searchForm.style.margin = '0 auto';
+searchForm.style.fontFamily = 'Montserrat';
+searchForm.style.fontSize = '16px';
+
+
+
+
+
+const searchInput = document.querySelector('input');
+console.log(searchInput);
+searchInput.style.width = '272px';
+searchInput.style.paddingLeft = '16px';
+searchInput.style.border = '1px solid #808080';
+searchInput.style.borderRadius = '4px';
+searchInput.style.color = '#808080';
+
+
+
+const searchButton = document.querySelector('button');
+console.log(searchButton);
+searchButton.style.width = '91px';
+searchButton.style.padding = '0px';
+searchButton.style.backgroundColor = '#4E75FF';
+searchButton.style.border = 'none';
+searchButton.style.borderRadius = '8px';
+searchButton.style.color = '#FFFFFF';
